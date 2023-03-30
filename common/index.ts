da9b67ad4d9f7768f8123a663f9c71b3c94d2734
pauft <1195566313@qq.com>
2023-03-31 00:38:48 +0800

    init

---
+++ common/index.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const a = axios.get('xxxx')
+
+export const b = axios.post('xxxx')
---
+++ web/vue-demo/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {b,a} from 'common'
+createApp(App).mount('#app')
