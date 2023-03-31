87c024ef6635fe02f935b81767b051904ad4c4f0
pauft <1195566313@qq.com>
2023-03-31 11:19:18 +0800

    wujie

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,30 +1,14 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div>
+    <h1>这是主应用</h1>
+    <WujieVue url="http://127.0.0.1:5174/" name="vue3"></WujieVue> <!--子应用vue3-->
+    <WujieVue url="http://127.0.0.1:5175/" name="react"></WujieVue> <!--子应用react-->
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang='ts'>
+import {ref,reactive} from 'vue'
+</script>
+
+<style scoped lang='less'>
 </style>
--- main/src/main.ts
+++ main/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Wujie from 'wujie-vue3' //引入一下引入对应的框架
 const app = createApp(App)
 
-app.use(router)
+
+
+app.use(router).use(Wujie) //注册一下
 
 app.mount('#app')
